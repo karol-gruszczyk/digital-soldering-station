a0863cbf7eb8a568214642bcf3921c5700959d45
karol-gruszczyk <karol.gruszczyk@gmail.com>
2017-02-21 23:28:20 +0100

    added schematics and iron controller logic

--- soldering_station/controller.h
+++ soldering_station/controller.h
@@ -1,18 +1,47 @@
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
 
-uint16_t current_iron_temperature = 0;
-uint16_t current_hot_air_temperature = 0;
+#define MAX_IRON_TEMPERATURE 400
+#define MIN_IRON_TEMPERATURE 100
+#define MAX_HOT_AIR_TEMPERATURE 400
+#define MIN_HOT_AIR_TEMPERATURE 100
 
+#define IRON_HEATER_PIN 0
+#define IRON_THERMISTOR_PIN A0
+#define IRON_REFERENCE_RESISTANCE 98.3
+#define IRON_THERMISTOR_MIN_RESISTANCE 50.0  // at room temperature - 20C
+#define IRON_THERMISTOR_MAX_RESISTANCE 200.0  // when iron is hot - 400C
+
+struct controller_t 
+{
+    uint16_t current_iron_temperature = 0;
+    uint16_t target_iron_temperature = 300;
+    float iron_thermistor_offset;
+    float iron_thermistor_factor;
+    
+    uint16_t current_hot_air_temperature = 0;
+    uint16_t target_hot_air_temperature = 300;
+    
+} controller;
 
 void controller_setup()
 {
-
+    float min_reading = IRON_THERMISTOR_MIN_RESISTANCE / (IRON_REFERENCE_RESISTANCE + IRON_THERMISTOR_MIN_RESISTANCE) * 1024;
+    float max_reading = IRON_THERMISTOR_MAX_RESISTANCE / (IRON_REFERENCE_RESISTANCE + IRON_THERMISTOR_MAX_RESISTANCE) * 1024;
+    float iron_range = max_reading - min_reading;
+    controller.iron_thermistor_factor = (400 - 20) / iron_range;  // temperature range from 20C to 400C
+    controller.iron_thermistor_offset = min_reading * controller.iron_thermistor_factor - 20;  // subtract room temperature
+    Serial.begin(9600);
 }
 
 void controller_loop()
 {
-
+    int raw_iron = analogRead(IRON_THERMISTOR_PIN);
+    controller.current_iron_temperature = raw_iron * controller.iron_thermistor_factor - controller.iron_thermistor_offset;
+    if (controller.current_iron_temperature < controller.target_iron_temperature)
+        digitalWrite(IRON_HEATER_PIN, HIGH);
+    else
+        digitalWrite(IRON_HEATER_PIN, LOW);
 }
 
 #endif /* CONTROLLER_H_ */
--- soldering_station/soldering_station.ino
+++ soldering_station/soldering_station.ino
@@ -1,4 +1,3 @@
-#include "settings.h"
 #include "controller.h"
 #include <LiquidCrystal.h>
 
@@ -7,8 +6,8 @@
 #define MULTI_FUNC_ENCODER_BUTTON 8
 
 LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
-enum mode_t {MODE_SELECT, IRON_TEMPERATURE, HOT_AIR_TEMPERATURE, HOT_AIR_SPEED};
-mode_t current_mode(IRON_TEMPERATURE), selected_mode(HOT_AIR_TEMPERATURE);
+enum mode_t {MODE_SELECT, IRON_TEMPERATURE, HOT_AIR_TEMPERATURE};
+mode_t current_mode(IRON_TEMPERATURE), selected_mode(IRON_TEMPERATURE);
 
 void multi_func_encoder();
 void lcd_update();
@@ -16,31 +15,30 @@ void print_mode(mode_t mode);
 
 void setup()
 {
-    load_settings();
+    controller_setup();
     lcd.begin(16, 2);
     print_mode(current_mode);
-    //attachInterrupt(digitalPinToInterrupt(MULTI_FUNC_ENCODER_PIN_1), multi_func_encoder, RISING);
-    //pinMode(MULTI_FUNC_ENCODER_PIN_2, INPUT);
-    //pinMode(MULTI_FUNC_ENCODER_BUTTON, INPUT);
+    pinMode(MULTI_FUNC_ENCODER_PIN_1, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(MULTI_FUNC_ENCODER_PIN_1), multi_func_encoder, FALLING);
+    pinMode(MULTI_FUNC_ENCODER_PIN_2, INPUT_PULLUP);
+    pinMode(MULTI_FUNC_ENCODER_BUTTON, INPUT_PULLUP);
 }
 
 void loop()
 {
-    if (digitalRead(MULTI_FUNC_ENCODER_BUTTON) == LOW && false)
+    controller_loop();
+    if (digitalRead(MULTI_FUNC_ENCODER_BUTTON) == LOW)
     {
         if (current_mode == MODE_SELECT)
-        {
             current_mode = selected_mode;
-            
-        }
         else
         {
             selected_mode = current_mode;
             current_mode = MODE_SELECT;
-            save_settings();
         }
         lcd.clear();
         print_mode(current_mode);
+        delay(250);
     }
 
     lcd_update();
@@ -51,23 +49,17 @@ void lcd_update()
     if (current_mode == MODE_SELECT)
     {
         if ((millis() % 1500) < 1000)
-        {
             print_mode(selected_mode);
-        }
         else
-        {
             lcd.clear();
-        }
     }
     else
     {
         lcd.setCursor(0, 1);
         if (current_mode == IRON_TEMPERATURE)
-            lcd.print("Temp: " + String(current_iron_temperature) + "C/" + String(settings.iron_target_temperature) + "C");
+            lcd.print("Temp: " + String(controller.current_iron_temperature) + "C/" + String(controller.target_iron_temperature) + "C");
         else if (current_mode == HOT_AIR_TEMPERATURE)
-            lcd.print("Temp: " + String(settings.hot_air_speed) + "%/" + String(MAX_HOT_AIR_SPEED) + "%");
-        else if (current_mode == HOT_AIR_SPEED)
-            lcd.print("Mode: Fan speed");
+            lcd.print("Temp: " + String(controller.current_hot_air_temperature) + "C/" + String(controller.target_hot_air_temperature) + "C");
     }
 }
 
@@ -78,67 +70,51 @@ void print_mode(mode_t mode)
         lcd.print("Mode: Iron");
     else if (mode == HOT_AIR_TEMPERATURE)
         lcd.print("Mode: Hot air");
-    else if (mode == HOT_AIR_SPEED)
-        lcd.print("Mode: Fan speed");
 }
+
+volatile bool interrupt_in_progress = false;
 
 void multi_func_encoder()
 {
-    bool direction = digitalRead(MULTI_FUNC_ENCODER_PIN_2) == HIGH;
+    if (interrupt_in_progress)
+        return;
+    interrupt_in_progress = true;
+    bool rot_direction = digitalRead(MULTI_FUNC_ENCODER_PIN_2) == HIGH;
+    delay(100);
     switch (current_mode)
     {
         case MODE_SELECT:
-            if (direction)
-            {
-                if (selected_mode == IRON_TEMPERATURE)
-                    selected_mode = HOT_AIR_TEMPERATURE;
-                else if (selected_mode == HOT_AIR_TEMPERATURE)
-                    selected_mode = HOT_AIR_SPEED;
-            }
-            else
-            {
-                if (selected_mode == HOT_AIR_SPEED)
-                    selected_mode = HOT_AIR_TEMPERATURE;
-                else if (selected_mode == HOT_AIR_TEMPERATURE)
-                    selected_mode = IRON_TEMPERATURE;
-            }
+            if (selected_mode == IRON_TEMPERATURE)
+                selected_mode = HOT_AIR_TEMPERATURE;
+            else if (selected_mode == HOT_AIR_TEMPERATURE)
+                selected_mode = IRON_TEMPERATURE;
+            lcd.clear();
             break;
         case IRON_TEMPERATURE:
-            if (direction)
+            if (rot_direction)
             {
-                if (++settings.iron_target_temperature > MAX_IRON_TEMPERATURE)
-                    settings.iron_target_temperature = MAX_IRON_TEMPERATURE;
+                if (++controller.target_iron_temperature > MAX_IRON_TEMPERATURE)
+                    controller.target_iron_temperature = MAX_IRON_TEMPERATURE;
             }
             else
             {
-                if (--settings.iron_target_temperature < MIN_IRON_TEMPERATURE)
-                    settings.iron_target_temperature = MIN_IRON_TEMPERATURE;
+                if (--controller.target_iron_temperature < MIN_IRON_TEMPERATURE)
+                    controller.target_iron_temperature = MIN_IRON_TEMPERATURE;
             }
             break;
         case HOT_AIR_TEMPERATURE:
-            if (direction)
+            if (rot_direction)
             {
-                if (++settings.hot_air_target_temperature > MAX_HOT_AIR_TEMPERATURE)
-                    settings.hot_air_target_temperature = MAX_HOT_AIR_TEMPERATURE;
+                if (++controller.target_hot_air_temperature > MAX_HOT_AIR_TEMPERATURE)
+                    controller.target_hot_air_temperature = MAX_HOT_AIR_TEMPERATURE;
             }
             else
             {
-                if (--settings.hot_air_target_temperature < MIN_HOT_AIR_TEMPERATURE)
-                    settings.hot_air_target_temperature = MIN_HOT_AIR_TEMPERATURE;
-            }
-            break;
-        case HOT_AIR_SPEED:
-            if (direction)
-            {
-                if (++settings.hot_air_speed > MAX_HOT_AIR_SPEED)
-                    settings.hot_air_speed = MAX_HOT_AIR_SPEED;
-            }
-            else
-            {
-                if (--settings.hot_air_speed < MIN_HOT_AIR_SPEED)
-                    settings.hot_air_speed = MIN_HOT_AIR_SPEED;
+                if (--controller.target_hot_air_temperature < MIN_HOT_AIR_TEMPERATURE)
+                    controller.target_hot_air_temperature = MIN_HOT_AIR_TEMPERATURE;
             }
             break;
     }
+    interrupt_in_progress = false;
 }
 
